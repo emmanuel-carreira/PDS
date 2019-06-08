4008d6ec05366989023c3829f3a16de7960b2070
Emmanuel Carreira <emmanuel_carreira@hotmail.com>
2019-06-08 17:52:35 -0300

    Added Q2_2.m code

---
+++ 2.2/Q2_2.m
@@ -0,0 +1,10 @@
+% Projeto PDS - Questão 2.2
+% Aluno: Emmanuel Carreira Alves
+% Objetivo: Contar automaticamente as regiões de diferentes texturas
+
+%Limpar variáveis de ambiente e janela de comando
+clear;
+clc;
+
+%Importar imagem
+[img, map] = imread('alumgrns.bmp');
