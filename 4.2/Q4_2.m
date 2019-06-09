f758feb7fde51343652aeeb312a0e0f197041fcb
Emmanuel Carreira <emmanuel_carreira@hotmail.com>
2019-06-09 15:17:59 -0300

    Ploted original signal and signal with noise

--- 4.2/Q4_2.m
+++ 4.2/Q4_2.m
@@ -12,3 +12,21 @@ clc;
 %Exibir a taxa de amostragem
 disp(Fs);
 
+%Criar sinal ruidoso
+noise_frequency = 466.16;
+w = 2*pi*noise_frequency;
+x_noise = 30*x + sin(w);
+
+%Exibir sinal original
+figure('Name', 'Comparação');
+subplot(1,2,1);
+t = linspace(0, length(x)/Fs, length(x));
+plot(t, x);
+title('Sinal original');
+
+%Exibir sinal ruidoso
+subplot(1,2,2);
+t = linspace(0, length(x_noise)/Fs, length(x_noise));
+plot(t, x_noise);
+title('Sinal ruidoso');
+
