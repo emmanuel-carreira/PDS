b7e8d68a7177e7b857fa87dabe13c1faf8ea336d
Emmanuel Carreira <emmanuel_carreira@hotmail.com>
2019-06-09 14:46:44 -0300

    Added Q1_2.m code

--- 1.2/Q1_2.m
+++ 1.2/Q1_2.m
@@ -31,4 +31,99 @@ end
 
 
 
-% sound(x_echo,Fs);
+% Reverberador com "a" negativo
+
+%Criar filtro reverberador
+b = 1;
+a = zeros(1,delay);
+a(1) = 1;
+
+%Filtar com a = -0.25
+a(delay) = -0.25;
+x_025 = filter(b, a, x_echo);
+
+%Filtar com a = -0.5
+a(delay) = -0.5;
+x_05 = filter(b, a, x_echo);
+
+%Filtar com a = -0.9
+a(delay) = -0.9;
+x_09 = filter(b, a, x_echo);
+
+%Exibir waveform do sinal original
+figure('Name', 'Comparação com reverberadores com a negativo');
+subplot(2,3,1);
+t = linspace(0, length(x)/Fs, length(x));
+plot(t, x);
+title('Sinal original');
+
+%Exibir waveform do sinal com eco
+subplot(2,3,2);
+t = linspace(0, length(x_echo)/Fs, length(x_echo));
+plot(t, x_echo);
+title('Sinal com eco');
+
+%Exibir waveform do sinal filtrado pelo reverberador a = -0.25
+subplot(2,3,3);
+t = linspace(0, length(x_025)/Fs, length(x_025));
+plot(t, x_025);
+title('Reverberador -0.25');
+
+%Exibir waveform do sinal filtrado pelo reverberador a = -0.5
+subplot(2,3,4);
+t = linspace(0, length(x_05)/Fs, length(x_05));
+plot(t, x_05);
+title('Reverberador -0.5');
+
+%Exibir waveform do sinal filtrado pelo reverberador a = -0.9
+subplot(2,3,5);
+t = linspace(0, length(x_09)/Fs, length(x_09));
+plot(t, x_09);
+title('Reverberador -0.9');
+
+
+
+% Reverberador com "a" positivo
+
+%Filtar com a = 0.25
+a(delay) = 0.25;
+x_025 = filter(b, a, x_echo);
+
+%Filtar com a = 0.5
+a(delay) = 0.5;
+x_05 = filter(b, a, x_echo);
+
+%Filtar com a = 0.9
+a(delay) = 0.9;
+x_09 = filter(b, a, x_echo);
+
+%Exibir waveform do sinal original
+figure('Name', 'Comparação com reverberadores com a positivo');
+subplot(2,3,1);
+t = linspace(0, length(x)/Fs, length(x));
+plot(t, x);
+title('Sinal original');
+
+%Exibir waveform do sinal com eco
+subplot(2,3,2);
+t = linspace(0, length(x_echo)/Fs, length(x_echo));
+plot(t, x_echo);
+title('Sinal com eco');
+
+%Exibir waveform do sinal filtrado pelo reverberador a = 0.25
+subplot(2,3,3);
+t = linspace(0, length(x_025)/Fs, length(x_025));
+plot(t, x_025);
+title('Reverberador 0.25');
+
+%Exibir waveform do sinal filtrado pelo reverberador a = 0.5
+subplot(2,3,4);
+t = linspace(0, length(x_05)/Fs, length(x_05));
+plot(t, x_05);
+title('Reverberador 0.5');
+
+%Exibir waveform do sinal filtrado pelo reverberador a = 0.9
+subplot(2,3,5);
+t = linspace(0, length(x_09)/Fs, length(x_09));
+plot(t, x_09);
+title('Reverberador 0.9');
